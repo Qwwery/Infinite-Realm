ef1a7ac86438de467e3137b21c949ea1c4a3f31f
end19 <regeneration76@yandex.ru>
2023-12-31 19:30:13 +0300

    fix.

--- draw_map.py
+++ draw_map.py
@@ -4,7 +4,7 @@ import pygame
 import os
 from maps import generation_map
 from heroes_file import Heroes
-from Box_file import get_boxes
+from box_file import get_boxes
 from doors_file import get_doors
 from pol_file import get_pols, Pol
 from walls_file import get_walls
